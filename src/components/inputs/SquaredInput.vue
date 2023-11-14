c24e169f2694033992a86c5eb950ef352e31db24
David Coimbra <daviddcoimbra@gmail.com>
2023-11-14 18:34:47 +0000

    feature/GZ-3 : Add Confirmation Code validation Modal

---
+++ src/components/inputs/SquaredInput.vue
@@ -0,0 +1,44 @@
+<script setup>
+ defineProps({
+  name: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: '',
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  value: {
+    type: String,
+    default: '',
+  },
+  placeholder: {
+    type: String,
+    default: '1'
+  }
+})
+
+const emits = defineEmits(['update:value'])
+
+const clearErrorAndEmit = (newValue) => {
+  emits('update:value', newValue);
+}
+</script>
+
+<template>
+    <input
+      :type="type"
+      :name="name"
+      :placeholder="placeholder"
+      :required="required"
+      v-bind="$attrs"
+      :value="value"
+      maxlength="1"
+      class="squared-input__rounded"
+      @input="clearErrorAndEmit($event.target.value)"
+    />
+</template>
