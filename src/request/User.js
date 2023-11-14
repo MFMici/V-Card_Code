2dcf87d8f745ab2552b5c5963b5b774eb47ad551
David Coimbra <daviddcoimbra@gmail.com>
2023-11-14 18:58:06 +0000

    feature/GZ-4 : Add User Requests

---
+++ src/request/User.js
@@ -0,0 +1,36 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, doc, setDoc, updateDoc, getDocs, query, where } from 'firebase/firestore';
+
+export default {
+    // One liners awaits, we can just await on the parent
+    async register(email, password, phone) {
+        try {
+            const response = await createUserWithEmailAndPassword(getAuth(), email, password);
+            const userDocRef = doc(collection(getFirestore(), 'users'), response.user.uid);
+
+            await setDoc(userDocRef, {
+                email,
+                phone,
+            });
+
+            return response
+        }
+        catch (error) {
+            console.error('Error registering user:', error.message);
+        }
+    },
+    update(data) {
+        const userDocRef = doc(collection(getFirestore(), 'users'), localStorage.getItem('token'))
+        return updateDoc(userDocRef, data)
+    },
+
+    async findBy(field, data) {
+        try {
+            const querySnapshot = await getDocs(query(collection(getFirestore(), 'users'), where(field, '==', data)));
+            return querySnapshot.docs.map(doc => doc.data());
+        }
+        catch (error) {
+            console.error('Error getting ' + field + ':', error.message);
+        }
+    }
+}
