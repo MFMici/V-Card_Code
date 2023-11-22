9e39d61c6cc13b8549b99a67884ba2f46942379b
David Coimbra <daviddcoimbra@gmail.com>
2023-11-22 05:47:48 +0000

    feature/GZ-22 : Add AddIcon

---
+++ src/components/icons/AddIcon.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg class="add-icon__position" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <path d="M10 19L10 1" stroke="#1A87DC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M1 10L19 10" stroke="#1A87DC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
