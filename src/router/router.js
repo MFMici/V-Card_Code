344ff019419cacdc4b2c038a64e8e20e52b882b6
David Coimbra <daviddcoimbra@gmail.com>
2023-11-22 00:29:43 +0000

    feature/GZ-17: Add route New Contact to router file

--- src/router/router.js
+++ src/router/router.js
@@ -1,3 +1,4 @@
+//import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -11,11 +12,18 @@ const routes = [
     name: 'Register',
     component: () => import('@/views/RegisterView.vue'),
   },
+  {
+    path: '/new-contact',
+    name: 'new-contact',
+    component: () => import('@/views/NewContact.vue'),
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+
 
 export default router
