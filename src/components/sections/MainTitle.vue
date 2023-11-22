238877f9beccbb7333b70e348a0f1efc132182dd
David Coimbra <daviddcoimbra@gmail.com>
2023-11-22 05:48:29 +0000

    feature/GZ-22 : Refactor MainTitle

--- src/components/sections/MainTitle.vue
+++ src/components/sections/MainTitle.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="contact__header__wrapper">
-    <BackButton @click="router.go(-1)" />
+    <router-link class="add-icon__position" v-if="type === 'N'" :to="{name: 'new-contact'}">
+      <AddIcon />
+    </router-link>
+    <BackButton v-else @click="router.go(-1)" />
     <h1 class="contact__title">{{title}}</h1>
   </div>
 </template>
 
 <script setup>
 import BackButton from "@/components/buttons/BackButton.vue";
+import AddIcon from "@/components/icons/AddIcon.vue";
 import { useRouter } from 'vue-router';
 
 defineProps({
@@ -14,6 +18,13 @@ defineProps({
     type: String,
     required: true,
   },
+  type: {
+        type: String,
+        default: 'C',
+        validator: (value) => {
+            return ['N', 'C'].includes(value);
+        },
+    },
 });
 
 const router = useRouter();
