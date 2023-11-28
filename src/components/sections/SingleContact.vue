ccb61b92b5e1fb50247fe1782ac6c15ec24bfe64
David Coimbra <daviddcoimbra@gmail.com>
2023-11-28 01:05:46 +0000

    feature/GZ-35: Refactor SingleContact name into member

--- src/components/sections/SingleContact.vue
+++ src/components/sections/SingleContact.vue
@@ -4,15 +4,15 @@
             <img class="single-contact__avatar" src="https://avatar.iran.liara.run/public/40" alt="avatar">
         </div>
         <div class="single-contact__info">
-            <div class="single-contact__name">
-                <h3>{{ name }}</h3>
-            </div>
             <div class="single-contact__phone">
                 <p>{{ phone }}</p>
             </div>
+            <div class="single-contact__member">
+                <h3>{{ member }}</h3>
+            </div>
         </div>
         <div class="single-contact__button">
-            <button class="cube-button"><LogoButtonIcon/></button>
+            <button v-if="member == 'V-Card User'" class="cube-button"><LogoButtonIcon/></button>
         </div>
     </div>
 </template>
@@ -20,7 +20,7 @@
 <script setup>
 import LogoButtonIcon from '@/components/icons/LogoButtonIcon.vue';
 defineProps({
-    name: {
+    member: {
         type: String,
         required: true,
     },
