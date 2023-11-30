079248f1d250917997c923c0e3f864d1a6f9d5bf
David Coimbra <daviddcoimbra@gmail.com>
2023-11-30 04:16:44 +0000

    feature/GZ-44: Add new route Settings to menu

--- src/router/router.js
+++ src/router/router.js
@@ -51,6 +51,14 @@ const routes = [
     meta: {
       requiresAuth: true
     },
+  },
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: () => import('@/views/SettingsView.vue'),
+    meta: {
+      requiresAuth: true
+    },
   }
 ]
 
