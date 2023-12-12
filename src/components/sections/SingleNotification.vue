c8e8144d83f0988d9b46d7126fe8d23fdacd9cb7
Ricardo Órfão <ricardorfao@gmail.com>
2023-12-12 23:51:34 +0000

    feature/GZ-96: add single notification item

---
+++ src/components/sections/SingleNotification.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="single-contact__container">
+        <MainTitle title="Notification" type="" class="mt-50" />
+        <div class="single-contact__info">
+            <div class="single-contact__phone">
+                <p>{{ title }}</p>
+            </div>
+            <div  class="single-contact__member">
+                <h3>{{ body }} </h3>
+            </div>
+        </div>
+        <div class="single-contact__button">
+            <button class="cube-button" ><WhiteEyeIcon/></button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+import WhiteEyeIcon from '@/components/icons/WhiteEyeIcon.vue'
+import { ref } from 'vue'
+
+
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+})
+
+const title = ref(props.title);
+const body = ref(props.body);
+
+</script>
